3e6e1a857447868ffc8165e3f926d3392d03288a
Amy Liu <amyliu1.618@gmail.com>
2021-01-18 19:35:02 -0700

    Platform generator (#2)
    * integrate platform generator to gameplay scene

---
+++ Assets/Scripts/Tools/Data.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public struct Tags  {
+    public static readonly string PLAYER = "Player";
+}
+
+public struct Pool
+{
+    public static readonly string PLATFORM = "Platform";
+} 
---
+++ Assets/Scripts/Tools/PlatformGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+    // [SerializeField]
+    // protected float initialSpawnRate = 1;
+    // protected float spawnRate;
+    // protected float lastSpawnTime;
+
+    private int platformCount;
+    [SerializeField]
+    private int maximumPlatformCount = 8;
+    
+    private GameObject platform;
+    [SerializeField]
+    private float spawnDistance = 200;
+    private PlatformController platformComponent;
+    private Vector3 platPosition;
+
+    [SerializeField]
+    private GameObject player;
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        // spawnRate = initialSpawnRate;
+        platformCount = 0;
+        platPosition = transform.position;
+        platform = ObjectPooler.Instance.SpawnFromPool(Pool.PLATFORM, platPosition, Quaternion.identity);
+        platPosition.z += platform.transform.localScale.z;
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        if (player.transform.position.z > platPosition.z - spawnDistance && platformCount < maximumPlatformCount)//Time.time > lastSpawnTime + spawnRate && platformCount < maximumPlatformCount)
+        {
+            // Set position of platform
+            platform = ObjectPooler.Instance.SpawnFromPool(Pool.PLATFORM, platPosition, Quaternion.identity);
+            platPosition.z += platform.transform.localScale.z;
+
+            platformComponent = platform.GetComponent<PlatformController>();
+            platformComponent.OnObjectSpawn();
+            platformComponent.onRemovePlatform += RemoveOne;
+
+            platformCount++;
+            // lastSpawnTime = Time.time;
+        }
+    }
+
+    
+    void RemoveOne()
+    {
+        platformCount--;
+    }
+}
