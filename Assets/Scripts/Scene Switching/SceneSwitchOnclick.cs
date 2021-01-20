d05517fc83475b9dc6a84ab4781749d52d329252
andrewting <andrewjamesting@yahoo.com>
2021-01-20 13:13:29 +0800

    Greatly improved scene transition scripting

---
+++ Assets/Scripts/Scene Switching/SceneSwitchOnclick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class SceneSwitchOnclick : MonoBehaviour
+{
+    // When a new scene is made, ensure the new scenetype enum value has the same name as the scene
+    #region Scene Enumerations
+    private enum SceneType {
+        Settings,
+        Game,
+        MainMenu
+    }
+    #endregion
+    
+    [SerializeField] private SceneType destinationScene = SceneType.MainMenu;
+    private Button thisButton;
+    void Start()
+    {
+        thisButton = GetComponent<Button>();
+        thisButton.onClick.AddListener(() => SceneManager.LoadScene(destinationScene.ToString()));
+    }
+}
