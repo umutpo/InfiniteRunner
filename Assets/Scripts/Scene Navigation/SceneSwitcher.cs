fe73cae1fe00976ccd664921fec30b8e99f109cd
Andrew Ting <andrewjamesting@yahoo.com>
2021-01-21 15:42:44 +0800

    Added scenes

---
+++ Assets/Scripts/Scene Navigation/SceneSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class SceneSwitcher : MonoBehaviour
+{
+    private enum SceneType {
+        MainMenu,
+        Settings,
+        GameplayScene
+    };
+
+    [SerializeField] private SceneType destinationScene;
+
+   public void Start() {
+       Button thisButton = GetComponent<Button>();
+       thisButton.onClick.AddListener(() => SceneManager.LoadScene(destinationScene.ToString()));
+    }
+}
