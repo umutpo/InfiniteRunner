871d7bb341008fd0080564ae786caaf9d73d6145
Amy Liu <amyliu1.618@gmail.com>
2021-01-26 16:16:35 -0700

    create obstacle generator scripts and prefabs

---
+++ Assets/Scripts/Tools/ObstacleController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour, IPooledObject
+{    
+    [SerializeField]
+    private GameObject player;
+
+    public delegate void ObstacleDelegate();
+    public ObstacleDelegate onRemoveObstacle;
+  
+    public virtual void OnObjectSpawn() {
+    }
+
+    protected virtual void Start() {
+        player = GameObject.Find("Player");
+    }
+
+    protected virtual void Update() {
+        if (false) {
+            // Remove and slow down player if collided
+        } else if (player.transform.position.z > transform.position.z + transform.localScale.z)
+            // Remove once out of camera view
+            Remove();
+    }
+
+    public virtual void Remove() {
+        if (onRemoveObstacle != null)
+        {
+            onRemoveObstacle.Invoke();
+        }
+        onRemoveObstacle = null;
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Tools/ObstacleGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    private int obstacleCount;
+    [SerializeField]
+    private int maximumObstacleCount = 8;
+    
+    private GameObject obstacle;
+    [SerializeField]
+    private float spawnDistance = 200;
+    private PlatformController obstacleComponent;
+    private Vector3 obstaclePosition;
+    const float laneWidth = 3.0f;
+
+    [SerializeField]
+    private GameObject player;
+
+    protected void Start()
+    {
+        // spawnRate = initialSpawnRate;
+        obstacleCount = 0;
+        obstaclePosition = transform.position;
+        obstacle = ObjectPooler.Instance.SpawnFromPool(Pool.PLATFORM, obstaclePosition, Quaternion.identity);
+        if (obstacle != null)
+        {
+            obstaclePosition.z += obstacle.transform.localScale.z;
+        }
+    }
+
+    protected void Update()
+    {
+        if (player.transform.position.z > obstaclePosition.z - spawnDistance && obstacleCount < maximumObstacleCount)//Time.time > lastSpawnTime + spawnRate && platformCount < maximumPlatformCount)
+        {
+            // Set position of platform
+            obstacle = ObjectPooler.Instance.SpawnFromPool(Pool.PLATFORM, obstaclePosition, Quaternion.identity);
+            obstaclePosition.z += obstacle.transform.localScale.z;
+
+            obstacleComponent = obstacle.GetComponent<PlatformController>();
+            obstacleComponent.OnObjectSpawn();
+            obstacleComponent.onRemovePlatform += RemoveOne;
+
+            obstacleCount++;
+            // lastSpawnTime = Time.time;
+        }
+    }
+
+    
+    void RemoveOne()
+    {
+        obstacleCount--;
+    }
+}
