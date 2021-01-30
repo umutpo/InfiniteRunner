527c0e3592a1332fb12c3eb5bc8987215b307958
Umut Polat <mutnba@gmail.com>
2021-01-29 18:15:38 -0800

    Ingredient Generator with 2 Ingredient Prefabs

---
+++ Assets/Scripts/Tools/Ingredients/IngredientController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IngredientController : MonoBehaviour, IPooledObject
+{
+    [SerializeField]
+    private GameObject player;
+    private PlayerController playerScript;
+
+    public delegate void IngredientDelegate();
+    public IngredientDelegate onRemoveIngredient;
+
+    public virtual void OnObjectSpawn()
+    {
+    }
+
+    protected virtual void Start()
+    {
+        player = GameObject.Find("Player");
+        playerScript = player.GetComponent<PlayerController>();
+    }
+
+    protected virtual void Update()
+    {
+        if (player.transform.position.z > transform.position.z + transform.localScale.z)
+            // Remove once out of camera view
+            Remove();
+    }
+
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Remove();
+        }
+    }
+
+    public virtual void Remove()
+    {
+        if (onRemoveIngredient != null)
+        {
+            onRemoveIngredient.Invoke();
+        }
+        onRemoveIngredient = null;
+        gameObject.SetActive(false);
+    }
+}
