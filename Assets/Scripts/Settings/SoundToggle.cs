e83615ae9f1e3773d1f266b8b645d580ddce7f19
Andrew Ting <andrewjamesting@yahoo.com>
2021-02-27 16:11:34 +0800

    Added toggling volume icon on main menu

---
+++ Assets/Scripts/Settings/SoundToggle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundToggle : MonoBehaviour
+{
+    [SerializeField] private Sprite unmutedSoundSprite;
+    [SerializeField] private Sprite mutedSoundSprite;
+    void Start()
+    {
+        Button thisButton = GetComponent<Button>();
+        thisButton.onClick.AddListener(ToggleSound);
+    }
+
+
+    void ToggleSound()
+    {
+        if (GetComponent<Image>().sprite == unmutedSoundSprite)
+            GetComponent<Image>().sprite = mutedSoundSprite;
+        else if (GetComponent<Image>().sprite == mutedSoundSprite)
+            GetComponent<Image>().sprite = unmutedSoundSprite;
+        else Debug.LogWarning("Volume icon is in an undefined sprite state. You might have forgotten to change or deleted the serialized variable sprite references.");
+    }
+}
