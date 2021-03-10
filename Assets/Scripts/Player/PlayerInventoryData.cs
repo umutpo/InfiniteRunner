2d0e226684afbe1309673784f1effdbe08dbf2aa
Andrew Ting <andrewjamesting@yahoo.com>
2021-03-10 17:56:01 +0800

    Rewrote player inventory in terms of recipes instead of collected items

--- Assets/Scripts/Player/PlayerInventoryData.cs
+++ Assets/Scripts/Player/PlayerInventoryData.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
@@ -6,11 +6,9 @@ using System;
 public class PlayerInventoryData : MonoBehaviour
 {
     [SerializeField]
-    private List<string> collectedIngredients;
-    private Dictionary<string, int> collectedIngredientsCounts;
-
-    [SerializeField]
     private List<RecipeController> recipes;
+
+    private List< Dictionary <string, int> > recipeList;
 
     public static Action<string, int, Sprite> AddIngredientEvent;
     public static Action<string, int> RemoveIngredientEvent;
@@ -21,27 +19,37 @@ public class PlayerInventoryData : MonoBehaviour
 
     void Start()
     {
-        collectedIngredients = new List<string>();
-        collectedIngredientsCounts = new Dictionary<string, int>();
+        recipeList = new List< Dictionary <string, int> >();
         recipeDisplayAnim = recipeDisplay.GetComponent<Animator>();
+        foreach (RecipeController recipe in recipes) 
+        {
+            Dictionary <string, int> curDict = new Dictionary <string, int>();
+            foreach (IngredientController ingredient in recipe.ingredients) 
+            {
+                curDict.Add(ingredient.ingredient, 0);
+            }
+            recipeList.Add(curDict);
+        }
     }
 
     // Returns number of ingredients used up if dish is created
     public int AddIngredient(string ingredient, Sprite inventoryImage)
     {
         bool shouldCheckRecipes = false;
-        if (collectedIngredients.Contains(ingredient))
+        int obtainedIngredientCount = 1;
+        foreach (Dictionary <string, int> recipeMap in recipeList) 
         {
-            collectedIngredientsCounts[ingredient]++;
+            bool thisRecipeCompleted = true;
+            if (recipeMap.ContainsKey(ingredient)) {
+                recipeMap[ingredient]++;
+                obtainedIngredientCount = recipeMap[ingredient];
+            }
+            foreach (KeyValuePair<string, int> ingredientAmount in recipeMap)
+                if (ingredientAmount.Value == 0)
+                    thisRecipeCompleted = false;
+            shouldCheckRecipes = shouldCheckRecipes || thisRecipeCompleted;
         }
-        else
-        {
-            collectedIngredients.Add(ingredient);
-            collectedIngredientsCounts.Add(ingredient, 1);
-            shouldCheckRecipes = true;
-        }
-
-        addIngredientUI(ingredient, inventoryImage);
+        addIngredientUI(ingredient, obtainedIngredientCount, inventoryImage);
 
         if (shouldCheckRecipes)
         {
@@ -53,66 +61,96 @@ public class PlayerInventoryData : MonoBehaviour
     
     public void RemoveIngredient(string ingredient)
     {
-        collectedIngredientsCounts[ingredient]--;
-
-        removeIngredientUI(ingredient);
-
-        if (collectedIngredientsCounts[ingredient] <= 0)
+        int ingredientAmount = 0;
+        foreach (Dictionary <string, int> recipeMap in recipeList) 
         {
-            collectedIngredients.Remove(ingredient);
-            collectedIngredientsCounts.Remove(ingredient);
+            if (recipeMap.ContainsKey(ingredient)) {
+                recipeMap[ingredient]--;
+                ingredientAmount = recipeMap[ingredient];
+            }
         }
+
+        removeIngredientUI(ingredient, ingredientAmount);
     }
 
     private int checkRecipes()
     {
-        foreach (RecipeController recipe in recipes) 
+        foreach (Dictionary <string, int> recipeMap in recipeList) 
         {
-            bool canCreateRecipe = recipe.ingredients.TrueForAll(doesContainIngredient);
-            if (canCreateRecipe)
+            bool thisRecipeCompleted = true;
+            foreach (KeyValuePair<string, int> ingredientAmount in recipeMap)
+                if (ingredientAmount.Value == 0)
+                    thisRecipeCompleted = false;
+            if (thisRecipeCompleted)
             {
-                foreach (IngredientController ingredientController in recipe.ingredients)
+                List <string> ingredientsToBeRemoved = new List<string>();
+                foreach (KeyValuePair<string, int> ingredientAmount in recipeMap)
                 {
-                    RemoveIngredient(ingredientController.ingredient);
+                    ingredientsToBeRemoved.Add(ingredientAmount.Key);
                 }
-                ScoreController.currentScore += recipe.ingredients.Count * 100;
+                foreach (string ingredient in ingredientsToBeRemoved) {
+                    RemoveIngredient(ingredient);
+                }
+                ScoreController.currentScore += recipeMap.Count * 100;
                 // Set animation for correct recipe
                 recipeDisplayAnim.SetTrigger("CreateBurger");
-                return recipe.ingredients.Count;
+                return recipeMap.Count;
             }
         }
         return 0;
     }
 
-    private bool doesContainIngredient(IngredientController ingredientController)
-    {
-        return collectedIngredients.Contains(ingredientController.ingredient);
-    }
-
-
-    private void addIngredientUI(string ingredient, Sprite inventoryImage)
+    private void addIngredientUI(string ingredient, int ingredientAmount, Sprite inventoryImage)
     {
         if (AddIngredientEvent != null && ingredient != null)
         {
-            AddIngredientEvent(ingredient, collectedIngredientsCounts[ingredient], inventoryImage);
+            AddIngredientEvent(ingredient, ingredientAmount, inventoryImage);
         }
     }
 
-    private void removeIngredientUI(string ingredient)
+    private void removeIngredientUI(string ingredient, int ingredientAmount)
     {
         if (RemoveIngredientEvent != null && ingredient != null)
         {
-            RemoveIngredientEvent(ingredient, collectedIngredientsCounts[ingredient]);
+            RemoveIngredientEvent(ingredient, ingredientAmount);
         }
     }
 
-    public Dictionary<string, int> GetCollectedIngredientsCounts()
+    public Dictionary <string, int> GetCollectedIngredientsCounts()
     {
-        return collectedIngredientsCounts;
+        Dictionary <string, int> inventoryItems = new Dictionary<string, int>();
+        foreach (Dictionary <string, int> recipeMap in recipeList) 
+        {
+            foreach (KeyValuePair<string, int> ingredientAmount in recipeMap)
+                if (!inventoryItems.ContainsKey(ingredientAmount.Key))
+                    inventoryItems.Add(ingredientAmount.Key, ingredientAmount.Value);
+        }
+        return inventoryItems;
+    }
+
+    // use this method to get the "pseudo priority queue" of recipes sorted by their ingredient completion rate
+    public List <Dictionary <string, int> > GetRecipeProgress() {
+        recipeList.Sort(ClosestToFinishFirst);
+        return recipeList;
     }
 
     public List<RecipeController> GetRecipes()
     {
         return recipes;
+    }
+
+    // recipe priority comparison rule; whichever needs the least number of ingredients to complete gets placed foremost
+    public int ClosestToFinishFirst(Dictionary<string, int> recipe1, Dictionary<string, int> recipe2) {
+        int missingIngredientCnt1 = 0;
+        int missingIngredientCnt2 = 0;
+        foreach (KeyValuePair<string, int> ingredientAmount in recipe1) {
+            if (ingredientAmount.Value == 0)
+                missingIngredientCnt1++;
+        }
+        foreach (KeyValuePair<string, int> ingredientAmount in recipe2) {
+            if (ingredientAmount.Value == 0)
+                missingIngredientCnt2++;
+        }
+        return missingIngredientCnt1 - missingIngredientCnt2;
     }
 }
