c0a74af1c6a1d542fd0965335965a80512cda0fe
Amy Liu <amyliu1.618@gmail.com>
2021-04-18 14:31:35 -0600

    refactor code

--- Assets/Scripts/Tools/Platform/KitchenGenerator.cs
+++ Assets/Scripts/Tools/Platform/KitchenGenerator.cs
@@ -14,53 +14,63 @@ public class KitchenGenerator : MonoBehaviour
     private PlatformController kitchenComponent;
     private Vector3 leftPosition;
     private Vector3 rightPosition;
+    // TODO: change according to model
+    private float kitchenLength = 20f;
 
     [SerializeField]
     private GameObject player;
 
     private string[] KITCHENS = {Pool.KITCHEN1, Pool.KITCHEN2, Pool.KITCHEN3, Pool.KITCHEN4, Pool.KITCHEN5, Pool.KITCHEN6};
+    private Vector3[] pos = new Vector3[2];
 
     protected void Start()
     {
         kitchenCount = 0;
         leftPosition = transform.position - new Vector3(9, 0, 0);
         rightPosition = transform.position + new Vector3(9, 0, 0);
+        pos[0] = leftPosition;
+        pos[1] = rightPosition;
         kitchen = ObjectPooler.Instance.SpawnFromPool(GetRandomKitchen(), leftPosition, Quaternion.identity);
         if (kitchen != null)
         {
-            leftPosition.z += kitchen.transform.localScale.z * 20f;
+            leftPosition.z += kitchen.transform.localScale.z * kitchenLength;
         }
         kitchen = ObjectPooler.Instance.SpawnFromPool(GetRandomKitchen(), rightPosition, Quaternion.identity);
         if (kitchen != null)
         {
-            rightPosition.z += kitchen.transform.localScale.z * 20f;
+            rightPosition.z += kitchen.transform.localScale.z * kitchenLength;
         }
     }
 
     protected void Update()
     {
-        float spawnRange = leftPosition.z - spawnDistance;
+        // if (player.transform.position.z > spawnRange && kitchenCount < maximumKitchenCount)
+        // {
+        //     // Set position of platform
+        //     kitchen = ObjectPooler.Instance.SpawnFromPool(GetRandomKitchen(), leftPosition, Quaternion.identity);
+        //     // Check if platform spawned
+        //     if (kitchen == null) return;   // Should never occur if pool size is larger than max
+        //     leftPosition.z += kitchen.transform.localScale.z * kitchenLength;
+
+        //     kitchenComponent = kitchen.GetComponent<PlatformController>();
+        //     kitchenComponent.OnObjectSpawn();
+        //     kitchenComponent.onRemovePlatform += RemoveOne;
+
+        //     kitchenCount++;
+        // }
+        AddKitchen(0);
+        AddKitchen(1);
+    }
+
+    private void AddKitchen(int posIndex) {
+        float spawnRange = pos[posIndex].z - spawnDistance;        
         if (player.transform.position.z > spawnRange && kitchenCount < maximumKitchenCount)
         {
             // Set position of platform
-            kitchen = ObjectPooler.Instance.SpawnFromPool(GetRandomKitchen(), leftPosition, Quaternion.identity);
+            kitchen = ObjectPooler.Instance.SpawnFromPool(GetRandomKitchen(), pos[posIndex], Quaternion.identity);
             // Check if platform spawned
             if (kitchen == null) return;   // Should never occur if pool size is larger than max
-            leftPosition.z += kitchen.transform.localScale.z * 20f;
-
-            kitchenComponent = kitchen.GetComponent<PlatformController>();
-            kitchenComponent.OnObjectSpawn();
-            kitchenComponent.onRemovePlatform += RemoveOne;
-
-            kitchenCount++;
-        }
-        if (player.transform.position.z > spawnRange && kitchenCount < maximumKitchenCount)
-        {
-            // Set position of platform
-            kitchen = ObjectPooler.Instance.SpawnFromPool(GetRandomKitchen(), rightPosition, Quaternion.identity);
-            // Check if platform spawned
-            if (kitchen == null) return;   // Should never occur if pool size is larger than max
-            rightPosition.z += kitchen.transform.localScale.z * 20f;
+            pos[posIndex].z += kitchen.transform.localScale.z * kitchenLength;
 
             kitchenComponent = kitchen.GetComponent<PlatformController>();
             kitchenComponent.OnObjectSpawn();
