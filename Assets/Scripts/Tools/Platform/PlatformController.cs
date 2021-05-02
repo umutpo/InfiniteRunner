0d44d0f08ea7875b73b74373fe8a0d84873c05a3
Amy Liu <amyliu1.618@gmail.com>
2021-05-02 13:22:12 -0600

    minor refactors

--- Assets/Scripts/Tools/Platform/PlatformController.cs
+++ Assets/Scripts/Tools/Platform/PlatformController.cs
@@ -3,14 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformController : ItemController
-{    
-    private float speedReduction = 4f;
-
-    protected override void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Player") {
-            // Slow down player speed
-            playerScript.SlowDown(speedReduction);
-        }
-    }
+{
 }
