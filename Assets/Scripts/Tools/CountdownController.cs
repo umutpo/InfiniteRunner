c281f9ae880e0d478147b114e97b8edce4205fc4
Amy Liu <amyliu1.618@gmail.com>
2021-05-16 13:40:25 -0600

    add countdown scripts

---
+++ Assets/Scripts/Tools/CountdownController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownController : MonoBehaviour
+{
+    public Text countdownText = null;
+
+    public bool isInCountdown = true;
+    private float timeRemaining;
+
+    private void Start() {
+        isInCountdown = true;
+        timeRemaining = 4;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInCountdown)
+        {                
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                if (timeRemaining > 3)  countdownText.text = "3";
+                else if (timeRemaining > 2) countdownText.text = "2";
+                else if (timeRemaining > 1) countdownText.text = "1";
+                else countdownText.text = "Start!";
+            }
+            else
+            {
+                countdownText.text = "";
+                isInCountdown = false;
+            }
+        }
+        
+    }
+}
