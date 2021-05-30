ee8cbaaa0bdac5f6eb91b16b80cec36fadf6e19e
ethan <Easonzyc_@outlook.com>
2021-05-31 00:12:53 +0800

    Implemented crossfade screen transition for scenes

---
+++ Assets/Scripts/ScreenTransition/LevelAnimator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelAnimator : MonoBehaviour
+{
+    public Animator screenTransition;
+    public void sceneTransition()
+    {
+        StartCoroutine(loadAnimation());
+    }
+
+    IEnumerator loadAnimation()
+    {
+        screenTransition.SetTrigger("start");
+        yield return new WaitForSeconds(1f);
+    }
+}
