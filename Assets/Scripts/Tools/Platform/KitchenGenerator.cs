2f563a59fdbe4d72de3a1623821a8d18022fe88b
Andrew Ting <andrewjamesting@yahoo.com>
2021-07-25 23:22:42 -0700

    Added slide under kitchen prefabs, fixed phasing through bug of certain obstacles

--- Assets/Scripts/Tools/Platform/KitchenGenerator.cs
+++ Assets/Scripts/Tools/Platform/KitchenGenerator.cs
@@ -28,8 +28,8 @@ public class KitchenGenerator : MonoBehaviour
 
     private string[] LEFT_KITCHENS = { Pool.KITCHEN1, Pool.KITCHEN2, Pool.KITCHEN3, Pool.KITCHEN4, Pool.KITCHEN5 };
     private string[] RIGHT_KITCHENS = {  Pool.KITCHEN6, Pool.KITCHEN7, Pool.KITCHEN8, Pool.KITCHEN9, Pool.KITCHEN10 };
-    private string[] OBSTACLE_LEFT_KITCHENS = { Pool.OBSTACLEKIT1, Pool.OBSTACLEKIT2, Pool.OBSTACLEKIT3, Pool.OBSTACLEKIT4, Pool.OBSTACLEKIT5 };
-    private string[] OBSTACLE_RIGHT_KITCHENS = { Pool.OBSTACLEKIT6, Pool.OBSTACLEKIT7, Pool.OBSTACLEKIT8, Pool.OBSTACLEKIT9, Pool.OBSTACLEKIT10 };
+    private string[] OBSTACLE_LEFT_KITCHENS = { Pool.OBSTACLEKIT1, Pool.OBSTACLEKIT2, Pool.OBSTACLEKIT3, Pool.OBSTACLEKIT4, Pool.OBSTACLEKIT5, Pool.OBSTACLEKIT11, Pool.OBSTACLEKIT12, Pool.OBSTACLEKIT13 };
+    private string[] OBSTACLE_RIGHT_KITCHENS = { Pool.OBSTACLEKIT6, Pool.OBSTACLEKIT7, Pool.OBSTACLEKIT8, Pool.OBSTACLEKIT9, Pool.OBSTACLEKIT10, Pool.OBSTACLEKIT14, Pool.OBSTACLEKIT15, Pool.OBSTACLEKIT16};
 
     private Vector3[] pos = new Vector3[2];
     private float[] obstacleEnds = {-1, -1};    // {left lane obstacle end, right lane obstacle end}
