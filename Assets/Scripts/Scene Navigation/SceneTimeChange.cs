a451a128ce2b8e5096c0abbbd30968a97494692d
ethan <Easonzyc_@outlook.com>
2021-08-11 00:31:24 +0800

    Updated Pause Menu Funcitonality
    1. Back to pause menu after returning from viewing in game canvas
    2. Added countdown after pause

--- Assets/Scripts/Scene Navigation/SceneTimeChange.cs
+++ Assets/Scripts/Scene Navigation/SceneTimeChange.cs
@@ -6,6 +6,12 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public class SceneTimeChange : MonoBehaviour
 {
+    private enum Handler
+    {
+        canvasToViewDuringPauseHandler,
+        PauseOrCoutinueHandler
+    }
+    [SerializeField] private Handler handler;
     private enum SetToGameState
     {
         Pause,
@@ -15,10 +21,22 @@ public class SceneTimeChange : MonoBehaviour
     [SerializeField] private GameObject pauseMenuCanvas;
     private AudioListener audioListener;
 
+    public GameObject canvasToViewDuringPause;
+    public GameObject countDownText;
+
+    private int countDownLength = 3;
+
     public void Start()
     {
         Button thisButton = GetComponent<Button>();
-        thisButton.onClick.AddListener(PauseOrContinueHandler);
+        if(handler == Handler.canvasToViewDuringPauseHandler)
+        {
+            thisButton.onClick.AddListener(canvasToViewDuringPauseHandler);
+        } else
+        {
+            thisButton.onClick.AddListener(PauseOrContinueHandler);
+        }
+
     }
 
     public void PauseOrContinueHandler()
@@ -31,10 +49,48 @@ public class SceneTimeChange : MonoBehaviour
                 pauseMenuCanvas.SetActive(true);
                 break;
             case SetToGameState.Continue:
-                Time.timeScale = 1f;
+                CountdownController countDownController = countDownText.GetComponent<CountdownController>();
                 AudioListener.pause = false;
                 pauseMenuCanvas.SetActive(false);
+                countDownController.StartCoroutine(Countdown(countDownLength, countDownController));
                 break;
         }
     }
+
+    public void canvasToViewDuringPauseHandler ()
+    {
+        switch (setToGameState)
+        {
+            case SetToGameState.Pause:
+                canvasToViewDuringPause.SetActive(true);
+                pauseMenuCanvas.SetActive(false);
+                break;
+            case SetToGameState.Continue:
+                canvasToViewDuringPause.SetActive(false);
+                pauseMenuCanvas.SetActive(true);
+                break;
+        }
+    }
+
+    IEnumerator Countdown(int seconds, CountdownController countdownController)
+    {
+        TMPro.TextMeshProUGUI countdownText = countdownController.countdownText;
+            int counter = seconds;
+            while (counter >= 0)
+            {
+                if (counter == 0)
+                {
+                    countdownText.text = "Start!";
+                }
+                else
+                {
+                    countdownText.text = counter.ToString();
+                }
+                yield return new WaitForSecondsRealtime(1);
+                counter--;
+            }
+            countdownText.text = "";
+            Time.timeScale = 1f;
+    }
+
 }
