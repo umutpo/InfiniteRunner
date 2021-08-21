6cc61bcdbc49e868c0de3ceb81dd289c93df9231
Umut Polat <mutnba@gmail.com>
2021-08-21 12:48:27 -0700

    Added splash scene

--- Assets/ComicsController.cs
+++ Assets/ComicsController.cs
@@ -47,7 +47,6 @@ public class ComicsController : MonoBehaviour
         timer += Time.deltaTime;
         if (timer > automaticSkipTime)
         {
-            Debug.Log("Update change");
             selectComicRoutine();
         }
     }
